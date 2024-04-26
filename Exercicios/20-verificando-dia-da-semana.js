0bf59b6e00df9232f44e878b1cb9390dd91571e1
Fabio Flauzino <fabio@mfrural.com.br>
2024-04-26 15:08:48 -0300

    subindo códigos

---
+++ Exercicios/20-verificando-dia-da-semana.js
@@ -0,0 +1,15 @@
+const hoje = new Date();
+const diaSemana = hoje.getDay();
+
+switch (diaSemana) {
+  case 0:
+  case 2:
+  case 4:
+  case 6:
+    console.log('Plantão');
+    break;
+  
+default:
+    console.log('Folga');
+    break;
+}
